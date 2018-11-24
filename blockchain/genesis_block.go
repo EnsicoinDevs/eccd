a22a636db534787db421d0040e95d2183e983b77
Chito <chitochi@proton.me>
2018-11-24 01:29:34 +0100

    improved genesis block

--- blockchain/genesis_block.go
+++ blockchain/genesis_block.go
@@ -2,17 +2,20 @@ package blockchain
 
 import (
 	"github.com/EnsicoinDevs/ensicoincoin/network"
+	"github.com/EnsicoinDevs/ensicoincoin/utils"
 	"time"
 )
 
 var genesisBlock = Block{
 	Msg: &network.BlockMessage{
 		Header: &network.BlockHeader{
-			Version:   0,
-			Flags:     []string{"ici cest limag"},
-			Timestamp: time.Date(2019, 8, 26, 23, 42, 0, 0, time.UTC),
-			Bits:      1,
-			Nonce:     42,
+			Version:        0,
+			Flags:          []string{"ici cest limag"},
+			Timestamp:      time.Date(2019, 8, 26, 23, 42, 0, 0, time.UTC),
+			HashPrevBlock:  utils.NewHash([]byte("olala")),
+			HashMerkleRoot: utils.NewHash([]byte("olali")),
+			Bits:           1,
+			Nonce:          42,
 		},
 	},
 }
